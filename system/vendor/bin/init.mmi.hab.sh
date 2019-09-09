fe6687151512fc960cb53bf14530db86ae5795d3
Alexa <45006100+alexa-v2@users.noreply.github.com>
2019-09-09 16:25:42 -0400

    Update init.mmi.hab.sh

--- system/vendor/bin/init.mmi.hab.sh
+++ system/vendor/bin/init.mmi.hab.sh
@@ -1,4 +1,4 @@
 #!/system/bin/sh
 # High assurance boot my ass
 echo "Killed proprietary bs"
-su -c log -p F - t MgskFixes "Nuked proprietary bullshit and (possibly) fixed Magisk" 
+su -c log -p F -t MgskFixes "Nuked proprietary bullshit and (possibly) fixed Magisk" 
